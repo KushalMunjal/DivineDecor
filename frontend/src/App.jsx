45064808789b6d2c71bc244fe4e930f310f393fb
= <kevalbehera@gmail.com>
2024-03-25 17:42:21 +0530

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
-import "./App.css";
-import HomePage from './components/HomePage';
+import { Home } from "~/pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "~/pages/Layout";
+import Cart from "~/pages/Cart";
+import Login from "~/pages/Login";
+import { productsData } from "~/api/Api";
+import Product from "~/components/Product";
 
-function App() {
-  return(
-    <HomePage />
-  )
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader: productsData,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return (
+    <div className="font-bodyFont">
+      <RouterProvider router={router} />
+    </div>
+  );
+};
 
 export default App;
