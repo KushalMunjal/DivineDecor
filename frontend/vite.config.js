3cdfdc3bf162a816bc40a7409a00c72624b47dca
= <kevalbehera@gmail.com>
2024-03-28 16:18:26 +0530

    restart

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteAliases } from 'vite-aliases'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteAliases()],
+  plugins: [react()],
 })
