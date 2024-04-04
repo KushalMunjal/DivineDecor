83ebb12a51d9795d1557331bf4dbac79fa95bb3b
= <kevalbehera@gmail.com>
2024-04-04 08:04:42 +0530

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,45 @@
 import React from 'react';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './pages/header/Navbar'; // Adjust the path if needed
 import Footer from './pages/footer/Footer'; // Adjust the path if needed
 import dummyData from './components/Product/dummydata';
 import ProductCard from './components/Product/Products'; // Adjust the path if needed
+
 import Login from './pages/login/login';
 import Home from './pages/home/home';
 import AboutUsPage from './pages/about/about';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <AboutUsPage/>
-      {/* <Home/> */}
-      {/* <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 p-4">
-        {dummyData.map((product, index) => (
-          <ProductCard
-            key={index}
-            name={product.name}
-            reviews={product.reviews}
-            price={product.price}
-            image={product.image}
-          />
-        ))}
-      </div> */}
-      <Footer />
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <Navbar />
+  //     <Home/>
+  //     {/* <Home/> */}
+  //     {/* <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 p-4">
+  //       {dummyData.map((product, index) => (
+  //         <ProductCard
+  //           key={index}
+  //           name={product.name}
+  //           reviews={product.reviews}
+  //           price={product.price}
+  //           image={product.image}
+  //         />
+  //       ))}
+  //     </div> */}
+  //     <Footer />
+  //   </div>
+  // );
+  return(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route index element={<Home />} />
+        <Route path="products" element={<ProductCard />} />
+        <Route path="*" element={<Login />} />
+      </Route>
+    </Routes>
+    </BrowserRouter>)
 }
 
 export default App;
--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,6 +1,10 @@
+import Navbar from '../header/Navbar';
+import Footer from '../footer/Footer';
+
 export default function Home() {
     return (
       <div className="bg-white">
+        <Navbar />
         <div className="mx-auto max-w-7xl py-4">
           <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
             <svg
@@ -50,6 +54,7 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <Footer />
       </div>
     )
   }
