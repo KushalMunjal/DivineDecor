44dff27078853c58fc7d81ea46a0caac09a2b246
= <kevalbehera@gmail.com>
2024-05-01 10:18:10 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('MongoDB connected...');
   } catch (error) {
