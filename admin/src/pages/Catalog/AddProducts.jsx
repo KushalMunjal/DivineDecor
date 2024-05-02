ba5a4ec24c56c2cf471dbbb2bb13ba3c2b3a0db1
Kushal Munjal <92504132+KushalMunjal@users.noreply.github.com>
2024-05-02 10:09:47 +0530

    updated admin panel

---
+++ admin/src/pages/Catalog/AddProducts.jsx
@@ -0,0 +1,12 @@
+import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
+import DefaultLayout from '../../layout/DefaultLayout';
+
+const AddProducts = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Add Products" />
+    </DefaultLayout>
+  );
+};
+
+export default AddProducts;
---
+++ admin/src/pages/Catalog/EditCategory.jsx
@@ -0,0 +1,12 @@
+import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
+import DefaultLayout from '../../layout/DefaultLayout';
+
+const EditCategory = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="Edit Category" />
+    </DefaultLayout>
+  );
+};
+
+export default EditCategory;
