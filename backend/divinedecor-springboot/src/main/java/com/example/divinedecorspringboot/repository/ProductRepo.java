a97c3708d1dbd3112335e17f0dc2b9f70a68bc45
Kushal Munjal <92504132+KushalMunjal@users.noreply.github.com>
2024-05-04 01:32:34 +0530

    products fetched and added product

--- backend/divinedecor-springboot/src/main/java/com/example/divinedecorspringboot/repository/ProductRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.divinedecorspringboot.repository;
-
-import com.example.divinedecorspringboot.model.ProductsSpring;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepo extends MongoRepository <ProductsSpring,Integer>{
-
-}
